53ac4b8a0e4d750bcc84c0e824fbfbab9c77677c
Ashley de Silva <ashleydesilva2002@gmail.com>
2022-02-05 23:59:46 +1100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, libero deleniti! Accusamus, omnis est? Ut, neque id dolorem at ipsam quasi harum corrupti, saepe amet voluptas fuga rem soluta quisquam omnis eos cupiditate maxime incidunt, repellendus quis quo perferendis porro.
+      </p>
+    </section>
+  )
+}
+
+export default About
